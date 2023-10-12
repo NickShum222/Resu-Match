061ae05dcddf4fd2b7d9c8cb799892bb1fbb0498
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-10-11 22:37:48 -0700

    Minor change

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-  <div className='flex'>
-    
-  </div>
+  <section className='w-full'>
+    <div className="">
+      
+    </div>
+  </section>
   )
 }
