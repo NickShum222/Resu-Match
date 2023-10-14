9580b9e023ce5439ef93c415c3d70f4ea8c88db0
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-10-13 23:17:11 -0700

    Update login page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-  <section className='w-full bg-black flex justify-start'>
+  <section className='w-full bg-black flex justify-start min-h-[100vh]'>
     <div className="font-bold text-white text-[2rem]">
       Resu <span>.me</span>
     </div>
