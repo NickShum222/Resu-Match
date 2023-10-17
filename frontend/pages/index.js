838d078ebe345a2f73b432327d02df09d6828bcf
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-10-16 22:25:48 -0700

    Added Login

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,7 +3,8 @@ export default function Home() {
   return (
   <section className='w-full bg-black flex justify-start min-h-[100vh]'>
     <div className="font-bold text-white text-[2rem]">
-      Resu <span className="text-[#BFBFBF]">.me</span>
+      Resu<span className="text-[#BFBFBF]">.me</span>
+      <div>Login</div>
     </div>
   </section>
   )
