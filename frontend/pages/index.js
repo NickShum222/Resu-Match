a0bdc4d4f5411cceae05c9cee1a0cf36a912e653
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-11-07 21:15:19 -0800

    Initialize commit backend

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,21 @@
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home").then(
+      response => response.json()
+    ).then(data => console.log(data)
+    )
+  }, [])
+
   return (
-  <section className='w-full bg-black flex justify-start min-h-[100vh]'>
-    <div className="font-bold text-white text-[2rem]">
-      Resu<span className="text-[#BFBFBF]">.me</span>
-      <div>Login</div>
-      <div>Username</div>
-      <div>Password</div>
-    </div>
-  </section>
-  )
+    <section className="w-full bg-black flex flex-col justify-center items-center min-h-[100vh]">
+      <div className="font-bold text-white text-[3rem]">
+        Resu<span className="text-[#BFBFBF]">.me</span>
+      </div>
+      <button className="bg-primary text-white uppercase font-semibold text-[24px] py-3 px-[70px] rounded-md transform  duration-300 ease-in-out hover:bg-transparent border-[2px] border-primary hover:text-primary">
+        Get Started
+      </button>
+    </section>
+  );
 }
