88a26bdef7f35ef4489386e3b24b0f51cd6c9956
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-11-23 23:16:53 -0800

    Added job model

--- backend/backend/base/models.py
+++ backend/backend/base/models.py
@@ -9,3 +9,10 @@ class Item(models.Model):
     def __str__(self):
         return self.name
 
+class Job(models.Model):
+
+
+    title = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    
+
